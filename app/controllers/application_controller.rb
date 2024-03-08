defcc831d2cdb7ecdad984e27f004d2769d55b12
Manas Rane <manas.rane@kellogg.northwestern.edu>
2024-03-08 02:31:24 +0000

    halfwaydone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
+    @user = User.find_by({ "id" => session["user_id"] })
     puts "------------------ code before every request ------------------"
   end
 end
